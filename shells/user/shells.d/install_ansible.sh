2b18c4e2af24fa0ec5ad4f0f1d43a0eec01ccfea
Ryota Funaki <ryota.funaki@ngv.jp>
2024-11-09 15:59:07 +0900

    Change base image

---
+++ shells/user/shells.d/install_ansible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (c) 2024 RFull Development
+# This source code is managed under the MIT license. See LICENSE in the project root.
+
+# Install development tools
+# See https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-and-upgrading-ansible-with-pip
+pip install --user ansible
+exit 0
